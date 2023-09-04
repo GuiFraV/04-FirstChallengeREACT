696915f83f1e209e74d32a191b436837beeee0a1
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-09-04 17:42:11 +0200

    exo fini

--- src/index.js
+++ src/index.js
@@ -5,16 +5,49 @@ import "./styles.css";
 function App() {
   return (
     <div className="card">
-      {/* <Avatar /> */}
+      <Avatar />
       <div className="data">
-        {/* <Intro /> */}
+        <Intro />
         {/* Should contain one Skill component
         for each web dev skill that you have,
         customized with props */}
-        {/* <SkillList /> */}
+        <SkillList />
       </div>
     </div>
   );
+}
+
+function Avatar(){
+  return <img className="avatar" src='gf.png' alt='Guillaume Franguiadakis'/>
+}
+
+function Intro(){
+  return <div>
+    <h1>Guillaume Franguiadakis</h1>
+    <p>Full StackDeveloper skilled</p>
+  </div>
+}
+
+function SkillList(){
+  return <div className="skill-list">
+    <Skill skill='React' emoji="👍" color='blue' />
+    <Skill skill='HTML+CSS' emoji="👍" color='red' />
+    <Skill skill='Svelte' emoji="👍" color='orange ' />
+    <Skill skill='Javascript' emoji="👍"  color='yellow' />
+  </div>
+}
+
+function Skill(props){
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>
+        {props.skill}
+      </span>
+      <span>
+       {props.emoji}
+      </span>
+    </div>
+  )
 }
 
 const rootElement = document.getElementById("root");
